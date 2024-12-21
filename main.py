90c7020ad0cb0817891ba1f3fe70c61d2a23b8f1
Andrzej Swaton <andrzej.swaton@gmail.com>
2024-12-21 16:16:16 +0100

    get matching with

--- main.py
+++ main.py
@@ -34,55 +34,63 @@ def get_tensor_image(img_bytes):
     img = K.color.bgr_to_rgb(img)
     return img.to(device)
 
+async def process_matching(img1, img2):
+  input_dict = {"image0": K.color.rgb_to_grayscale(img1),
+          "image1": K.color.rgb_to_grayscale(img2)}
+
+  with torch.no_grad():
+    correspondences = matcher(input_dict)
+
+  mkpts0 = correspondences['keypoints0'].cpu().numpy()
+  mkpts1 = correspondences['keypoints1'].cpu().numpy()
+  Fm, inliers = cv2.findFundamentalMat(mkpts0, mkpts1, cv2.USAC_MAGSAC, 0.5, 0.999999, 10000)
+  inliers = inliers > 0
+
+  # Determine the number of matches to display
+  num_matches = min(mkpts0.shape[0], 20)
+
+  # Select a random subset of 20 matches if there are more than 20 matches
+  if mkpts0.shape[0] > 20:
+    selected_indices = torch.randperm(mkpts0.shape[0])[:num_matches]
+  else:
+    selected_indices = torch.arange(mkpts0.shape[0])
+
+  # Use the selected indices to filter matches
+  filtered_mkpts0 = mkpts0[selected_indices]
+  filtered_mkpts1 = mkpts1[selected_indices]
+  filtered_inliers = inliers[selected_indices] if inliers is not None else None
+
+  # Now, use the filtered matches in draw_LAF_matches
+  fig, ax = draw_LAF_matches(
+    KF.laf_from_center_scale_ori(torch.from_numpy(filtered_mkpts0).view(1, -1, 2),
+                  torch.ones(num_matches).view(1, -1, 1, 1),
+                  torch.ones(num_matches).view(1, -1, 1)),
+    KF.laf_from_center_scale_ori(torch.from_numpy(filtered_mkpts1).view(1, -1, 2),
+                  torch.ones(num_matches).view(1, -1, 1, 1),
+                  torch.ones(num_matches).view(1, -1, 1)),
+    torch.arange(num_matches).view(-1,1).repeat(1,2),  # Adjusted indices for drawing
+    K.tensor_to_image(img1),
+    K.tensor_to_image(img2),
+    filtered_inliers,  # Use the filtered inliers
+    draw_dict={'inlier_color': (0.2, 1, 0.2),
+          'tentative_color': None,
+          'feature_color': (0.2, 0.5, 1), 'vertical': False}, return_fig_ax=True)
+
+  ax.axis('off')
+  plt.savefig('output.jpg', bbox_inches='tight')
+  return FileResponse("./output.jpg", media_type="image/jpeg")
+
+@app.post("/get_matching_with")
+async def get_matching_with(image1: UploadFile = File(...), image_path: str = ""):
+  img1 = get_tensor_image(await image1.read())
+  img2 = get_tensor_image(open(image_path, "rb").read())
+  return await process_matching(img1, img2)
+
 @app.post("/get_matching")
 async def get_matching(image1: UploadFile = File(...), image2: UploadFile = File(...)):
-    img1 = get_tensor_image(await image1.read())
-    img2 = get_tensor_image(await image2.read())
-
-    input_dict = {"image0": K.color.rgb_to_grayscale(img1), 
-              "image1": K.color.rgb_to_grayscale(img2)}
-
-    with torch.no_grad():
-        correspondences = matcher(input_dict)
-
-    mkpts0 = correspondences['keypoints0'].cpu().numpy()
-    mkpts1 = correspondences['keypoints1'].cpu().numpy()
-    Fm, inliers = cv2.findFundamentalMat(mkpts0, mkpts1, cv2.USAC_MAGSAC, 0.5, 0.999999, 10000)
-    inliers = inliers > 0   
-
-    # Determine the number of matches to display
-    num_matches = min(mkpts0.shape[0], 20)
-
-    # Select a random subset of 20 matches if there are more than 20 matches
-    if mkpts0.shape[0] > 20:
-        selected_indices = torch.randperm(mkpts0.shape[0])[:num_matches]
-    else:
-        selected_indices = torch.arange(mkpts0.shape[0])
-
-    # Use the selected indices to filter matches
-    filtered_mkpts0 = mkpts0[selected_indices]
-    filtered_mkpts1 = mkpts1[selected_indices]
-    filtered_inliers = inliers[selected_indices] if inliers is not None else None
-
-    # Now, use the filtered matches in draw_LAF_matches
-    fig, ax = draw_LAF_matches(
-        KF.laf_from_center_scale_ori(torch.from_numpy(filtered_mkpts0).view(1, -1, 2),
-                                    torch.ones(num_matches).view(1, -1, 1, 1),
-                                    torch.ones(num_matches).view(1, -1, 1)),
-        KF.laf_from_center_scale_ori(torch.from_numpy(filtered_mkpts1).view(1, -1, 2),
-                                    torch.ones(num_matches).view(1, -1, 1, 1),
-                                    torch.ones(num_matches).view(1, -1, 1)),
-        torch.arange(num_matches).view(-1,1).repeat(1,2),  # Adjusted indices for drawing
-        K.tensor_to_image(img1),
-        K.tensor_to_image(img2),
-        filtered_inliers,  # Use the filtered inliers
-        draw_dict={'inlier_color': (0.2, 1, 0.2),
-                  'tentative_color': None,
-                  'feature_color': (0.2, 0.5, 1), 'vertical': False}, return_fig_ax=True)
-
-    ax.axis('off')
-    plt.savefig('output.jpg', bbox_inches='tight')
-    return FileResponse("./output.jpg", media_type="image/jpeg")
+  img1 = get_tensor_image(await image1.read())
+  img2 = get_tensor_image(await image2.read())
+  return await process_matching(img1, img2)
 
 async def getSimilarityScore(tensorImage1, tensorImage2):
     input_dict = {"image0": K.color.rgb_to_grayscale(tensorImage1),
