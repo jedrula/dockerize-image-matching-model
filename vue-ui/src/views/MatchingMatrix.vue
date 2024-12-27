3b05704839f3ed3663d9b37f8770653bed806b6f
Andrzej Swaton <andrzej.swaton@gmail.com>
2024-12-27 01:11:30 +0100

    interactive matching matrix (onclick line drawing)

--- vue-ui/src/views/MatchingMatrix.vue
+++ vue-ui/src/views/MatchingMatrix.vue
@@ -18,20 +18,41 @@
           :src="`${apiUrl}/${matchingMatrixResult.image2.path}`"
           alt="image 2"
         />
-        <svg :width="svgWidth" :height="svgHeight">
-          <template v-if="svgWidth && svgHeight">
-            <line
-              v-for="(line, index) in lines"
-              :key="index"
-              :x1="line.x1"
-              :y1="line.y1"
-              :x2="line.x2"
-              :y2="line.y2"
-              :stroke="line.stroke"
-              stroke-width="3"
-            />
-          </template>
-        </svg>
+        <div
+          class="svg-wrapper"
+          :style="{ width: svgWidth + 'px', height: svgHeight + 'px' }"
+        >
+          <svg :width="svgWidth" :height="svgHeight">
+            <template v-if="svgWidth && svgHeight">
+              <template v-for="(line, index) in lines" :key="`circle-${index}`">
+                <circle
+                  :cx="line.x1"
+                  :cy="line.y1"
+                  r="6"
+                  fill="green"
+                  @click="circleClicked(line)"
+                />
+                <circle
+                  :cx="line.x2"
+                  :cy="line.y2"
+                  r="6"
+                  fill="green"
+                  @click="circleClicked(line)"
+                />
+              </template>
+              <line
+                v-for="(line, index) in shownLines"
+                :key="`line-${index}`"
+                :x1="line.x1"
+                :y1="line.y1"
+                :x2="line.x2"
+                :y2="line.y2"
+                :stroke="line.stroke"
+                stroke-width="3"
+              />
+            </template>
+          </svg>
+        </div>
       </div>
       <pre>{{
         JSON.stringify(matchingMatrixResult.matched_points, null, 2)
@@ -79,9 +100,19 @@ const lines = computed(() => {
       x2: point2.x + image1.value.width,
       y2: point2.y,
       stroke: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+      show: ref(false),
     };
   });
 });
+
+const shownLines = computed(() =>
+  lines.value.filter((line) => line.show.value)
+);
+
+const circleClicked = (line) => {
+  console.log("circle clicked", line);
+  line.show.value = !line.show.value;
+};
 </script>
 
 <style scoped>
@@ -96,11 +127,17 @@ const lines = computed(() => {
   display: flex;
 }
 
+.svg-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+}
+
 svg {
   position: absolute;
   top: 0;
   left: 0;
-  pointer-events: none;
 }
 
 pre {
